0beff539b520cfcd8ec42da8ce8985f7f54cf0f0
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2023-11-29 23:42:21 +0530

    Program to print largest of three numbers

---
+++ largest.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b,c;
+	
+	cout<<"Enter first number :";
+	cin>>a;
+	
+	cout<<"Enter second number :";
+	cin>>b;
+	
+	cout<<"Enter third number :";
+	cin>>c;
+	
+	if (a>b && a>c)
+	cout<<"\nFirst number i.e. "<<a<<" is the largest.";
+	
+	else if ( b>a && b>c)
+	cout<<"\nSecond number i.e. "<<b<<" is the largest.";
+	
+	else
+	cout<<"\nThird number i.e. "<<c<<" is the largest.";
+	
+}
