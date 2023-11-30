0a0b9ef692b3a515a4e3410dffc331f8594ee7ee
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2023-11-30 20:07:14 +0530

    Program to check the entered number is positive or negative

---
+++ check_nature.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	
+	cout<<"Enter a number :";
+	cin>>a;
+	
+	if(a>0)
+	cout<<"The entered number is a positive number !!";
+	
+	else if ( a<0)
+	cout<<"The entered number is a negative number !!";
+	
+	else
+	cout<<"The entered number is 0 ";
+}
