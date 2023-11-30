f7b477e6d22e018f022715b4180ff9967401534e
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2023-11-30 20:10:40 +0530

    Program to check the number is even or odd

---
+++ even_odd.cpp
@@ -0,0 +1,16 @@
+#include<iostream >
+using namespace std;
+
+int main()
+{
+	int n;
+	
+	cout<<"Enter a number :";
+	cin>>n;
+	
+	if ( n%2==0)
+	cout<<"Entered number is an even number !!";
+	
+	else
+	cout<<"Entered number is an odd number !!";
+}
