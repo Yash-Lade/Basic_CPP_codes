2cc4895a458665096a625a603369cc730a45c824
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2023-12-01 22:25:09 +0530

    string datatype added

--- sizeofdatatypes.cpp
+++ sizeofdatatypes.cpp
@@ -1,14 +1,17 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 int main()
 {
-	int a;
+    int a;
+    string b;
     
     cout<<"\nThe size of integer is :"<<sizeof(a)<<endl;
     cout<<"The size of float is :"<<sizeof(float(a))<<endl;
     cout<<"The size of double is :"<<sizeof(double(a))<<endl;
     cout<<"The size of char is :"<<sizeof(char(a))<<endl;
+    cout<<"The size of string is :"<<sizeof(b)<<endl;
 }
     
 	
