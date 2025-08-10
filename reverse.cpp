a2df4afaa341e6c80dcbf8c6e33bae3b8c49fc58
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2025-08-10 23:15:21 +0530

    Update reverse.cpp

--- reverse.cpp
+++ reverse.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
+#include<climits>
 using namespace std;
 
+int revno(int n){
+	int lastdig=0;
+	long long rev=0;
+
+	while(n>0){
+		lastdig=n%10;
+        n=n/10;
+        rev=(rev*10)+lastdig;
+		if(rev> INT_MAX || rev < INT_MIN) return 0;
+	}
+	return rev;
+}
 int main()
 {
 	int n,rev=0,temp;
@@ -8,13 +21,8 @@ int main()
 	cout<<"Enter a number :";
 	cin>>n;
 	
-	temp=n;
-	while(n>0)
-	{
-		rev=rev*10+n%10;
-		
-		n=n/10;
-	}
+	int rev=revno(n);
 	
-	cout<<"The reverse of "<<temp<<" is "<<rev;
+	cout<<"The reverse is"<<rev;
 }
+
